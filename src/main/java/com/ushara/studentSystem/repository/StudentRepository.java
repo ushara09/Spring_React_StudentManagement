97fe8a6ec4888600a42ce3ef41a1b63fa0bc3994
ushara09 <thimalushara@gmail.com>
2021-12-23 20:41:18 +0530

    connected to the database

--- src/main/java/com/ushara/studentSystem/repository/StudentRepository.java
+++ src/main/java/com/ushara/studentSystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.ushara.studentSystem.repository;
 
-public interface StudentRepository {
+import com.ushara.studentSystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
